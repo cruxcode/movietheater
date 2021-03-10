23b148ac12455b9d3becb42bb97c18db1f73f2a2
cruxcode <swaroopshyam0@gmail.com>
2021-03-10 01:57:03 -0500

    reading input file successfully

--- src/main/java/movie/App.java
+++ src/main/java/movie/App.java
@@ -1,6 +1,8 @@
 package movie;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,42 +19,39 @@ public class App
         	logger.fatal("exactly one argument is required");
         	return;
         }
-        File inputFile = new File(args[0]);
-        if(!inputFile.exists()) {
-        	logger.fatal("input file does not exist");
-        	return;
-        }
-        
         Integer numRows = 10;
         Integer numCols = 20;
 
         
         allocator = new BaselineAllocator();
         allocator.setSize(numRows, numCols);
-        AllocatorResult result = allocator.allocate(5);
-        if(result != null) {
-        	System.out.println(result.toString());
-        }
-        result = allocator.allocate(3);
-        if(result != null) {
-        	System.out.println(result.toString());
-        }
-        result = allocator.allocate(3);
-        if(result != null) {
-        	System.out.println(result.toString());
-        }
-        result = allocator.allocate(1);
-        if(result != null) {
-        	System.out.println(result.toString());
-        }
-        result = allocator.allocate(10);
-        if(result != null) {
-        	System.out.println(result.toString());
-        }
-        result = allocator.allocate(15);
-        if(result != null) {
-        	System.out.println(result.toString());
-        }
+        File inputFile = new File(args[0]);
+        String[] splits = null;
+        try {
+			Scanner reader = new Scanner(inputFile);
+			while(reader.hasNextLine()) {
+				String line = reader.nextLine();
+				splits = line.split(" ");
+				if(splits.length < 2) {
+					logger.fatal("bad input file");
+					return;
+				}
+		        String reqName = splits[0];
+		        Integer numSeats = null;
+		        try {
+		        	numSeats = Integer.parseInt(splits[1]);
+		        	AllocatorResult result = allocator.allocate(numSeats);
+		        	if(result != null) {
+		            	System.out.println(result.toString());
+		            }
+		        } catch (Exception e) {
+		        	logger.fatal("second split should be an integer");
+		        }
+			}
+		} catch (FileNotFoundException e) {
+			logger.fatal("input file does not exist");
+			return;
+		}
         logger.info("stopping simulation...");
     }
 }
