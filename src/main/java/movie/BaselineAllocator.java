6136e33e4d9f9540170e3d918cf17b5239dd705f
cruxcode <swaroopshyam0@gmail.com>
2021-03-09 21:42:00 -0500

    Implementing baseline algorithm

--- src/main/java/movie/BaselineAllocator.java
+++ src/main/java/movie/BaselineAllocator.java
@@ -1,5 +1,7 @@
 package movie;
 
+import java.util.LinkedList;
+
 public class BaselineAllocator implements AllocatorStrategy {
 	private PsuedoMatrix matrix;
 	@Override
@@ -9,8 +11,12 @@ public class BaselineAllocator implements AllocatorStrategy {
 
 	@Override
 	public AllocatorResult allocate(Integer numSeats) {
-		// TODO Auto-generated method stub
-		return null;
+		AllocatorResult result = null;
+		for (Integer i = 0; i < this.matrix.getNumRows(); i++) {
+			LinkedList<FreeRange> row = this.matrix.getRow(i);
+			
+		}
+		return result;
 	}
 
 }
